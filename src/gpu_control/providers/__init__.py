591998a58d5b04c97f361a186f37d28ca4456f81
Unjuno <unjuno.org@unjuno.org>
2026-08-28 07:51:47 +0900

    Export provider result finalization contracts

--- src/gpu_control/providers/__init__.py
+++ src/gpu_control/providers/__init__.py
@@ -18,6 +18,13 @@ from .controller import (
     observe_provider_job,
     submit_approved_plan,
 )
+from .finalization import (
+    FinalizedCapturedResults,
+    ProviderResultCapture,
+    capture_provider_results_before_cleanup,
+    finalize_captured_provider_results,
+    validate_result_capture_against_lifecycle,
+)
 from .runpod_adapter import RunPodV2Adapter, RunPodV2AdapterError
 from .runpod_occupancy import RunPodAccountOccupancyEvidence, build_account_occupancy_evidence
 from .runpod_pricing import (
@@ -38,9 +45,11 @@ from .runpod_v2 import (
 from .synthetic import SyntheticProviderAdapter
 
 __all__ = [
+    "FinalizedCapturedResults",
     "ProviderAdapter",
     "ProviderCleanupSnapshot",
     "ProviderContractError",
+    "ProviderResultCapture",
     "ProviderResultSnapshot",
     "ProviderStatusSnapshot",
     "ProviderSubmission",
@@ -58,11 +67,14 @@ __all__ = [
     "build_catalog_pricing_evidence",
     "build_create_pod_payload",
     "build_priced_create_pod_payload",
+    "capture_provider_results_before_cleanup",
     "cleanup_provider_job",
     "collect_provider_results",
+    "finalize_captured_provider_results",
     "observe_provider_job",
     "submit_approved_plan",
     "translate_pod_status",
     "validate_created_pod",
     "validate_created_pod_with_pricing",
+    "validate_result_capture_against_lifecycle",
 ]
